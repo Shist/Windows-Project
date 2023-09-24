86a834643dbca8619113d50f0aa609a5fcf263f7
Shist <shist.pupust@mail.ru>
2023-09-24 19:46:49 +0300

    Added separate id and selector for popup which opens in 60 sec

--- dist/script.js
+++ dist/script.js
@@ -326,7 +326,7 @@ const modals = () => {
   bindModal("width-height_form", ".popup_calc_btn", ".popup_calc", ".popup_calc_close");
   bindModal("checkbox_form", ".popup_calc_button", ".popup_calc_profile", ".popup_calc_profile_close", false);
   bindModal("final_form", ".popup_calc_profile_button", ".popup_calc_end", ".popup_calc_end_close", false);
-  showModalByTime(".popup", 60000);
+  showModalByTime("#popup-request-call", 60000);
 };
 /* harmony default export */ __webpack_exports__["default"] = (modals);
 
